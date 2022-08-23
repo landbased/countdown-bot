9adf0b4e96ae23e159146257b30bc82637c0e4a9
David Kroek <dkroek@gmail.com>
2022-08-23 23:38:09 +1000

    init

---
+++ letters.mjs
@@ -0,0 +1,113 @@
+import { readFile } from 'node:fs/promises';
+
+const permutationsOf = (array) => {
+  const swap = (array, pos1, pos2) => {
+    var temp = array[pos1];
+    array[pos1] = array[pos2];
+    array[pos2] = temp;
+  };
+
+  const heaps = (n, heapArr, results = []) => {
+    if (n < 1) {
+      results.push(heapArr.slice());
+      return;
+    }
+
+    heaps(n-1, heapArr, results);
+    for (let i = 0; i < n-1; i++) {
+      if (n % 2 === 0) {
+        swap(heapArr, i, n-1);
+      } else {
+        swap(heapArr, 0, n-1);
+      }
+
+      heaps(n-1, heapArr, results);
+    }
+    return results;
+  }
+
+  if (typeof(array) === 'string') {
+    return heaps(array.length, array.split(''), []);
+  } else if (Array.isArray(array)) {
+    return heaps(array.length, array.slice(), []);
+  } else {
+    console.error(`called permutations with a non array arg: ${array}`);
+    return [];
+  }
+}
+
+class Trie {
+  constructor() {
+    this.root = {};
+  }
+
+  insert(word) {
+    let node = this.root;
+    for (let i = 0; i < word.length; i++) {
+      const char = word[i];
+      if (!node[char]) {
+        node[char] = {};
+      }
+      node = node[char];
+      if (i === word.length-1) {
+        node.END = true;
+      }
+    }
+  }
+
+  contains(word) {
+    let node = this.root;
+    for (let i = 0; i < word.length; i++) {
+      const char = word[i];
+      if (!node[char]) {
+        return false;
+      }
+      node = node[char];
+    }
+    return !!node.END;
+  }
+}
+
+// LETTERS
+const MIN_WORD_LENGTH = 4;
+
+const args = process.argv.slice(2);
+
+const letters = args[0];
+console.log(letters);
+
+const t1 = performance.now();
+
+const rawWords = await readFile('./words.txt');
+
+const wordsList = String(rawWords).split(/\r?\n/);
+
+const wordsTrie = new Trie();
+for (const word of wordsList) {
+  wordsTrie.insert(word);
+}
+
+const letterPermutations = permutationsOf(letters).map(w => w.join(''));
+
+
+
+const answers = new Set();
+
+for (const perm of letterPermutations) {
+  let word = perm;
+  while (word.length >= MIN_WORD_LENGTH) {
+    if (wordsTrie.contains(word)) {
+      answers.add(word);
+      break;
+    }
+    word = word.slice(0, -1);
+  }
+}
+
+const sorted = Array.from(answers).sort((a,b) => b.length - a.length);
+
+const t2 = performance.now();
+
+console.log(sorted);
+
+console.log(`took ${t2-t1}ms`); 
